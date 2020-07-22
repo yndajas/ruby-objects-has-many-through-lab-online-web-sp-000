c29eea5892de9900ed5083eb31b8e13a709176dc
Ynda Jas <yndajas@gmail.com>
2020-07-22 16:23:03 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,25 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+  
+  def new_appointment(date, patient)
+    appointment = Appointment.new
+    appointment.date = date
+    appointment.patient = patient
+    appointment.doctor = self
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment|appointment.doctor == self}
+  end
+end
