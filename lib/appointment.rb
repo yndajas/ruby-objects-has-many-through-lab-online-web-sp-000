626d8c2e418ccf603e4a6da7d03d621e2040966b
Ynda Jas <yndajas@gmail.com>
2020-07-22 16:29:17 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,17 @@
 class Appointment
-  def initialize(date, patient, doctor)
+  attr_accessor :date, :patient, :doctor
+  
+  @@all = []
+  
+  def self.all
+    @@all
   end
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
 end
