d484603191d7c7d2e27180e167d7c0a368d2c3ee
Ynda Jas <yndajas@gmail.com>
2020-07-22 16:27:21 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,10 +13,7 @@ class Doctor
   end
   
   def new_appointment(date, patient)
-    appointment = Appointment.new
-    appointment.date = date
-    appointment.patient = patient
-    appointment.doctor = self
+    appointment = Appointment.new(date, patient, self)
   end
   
   def appointments
