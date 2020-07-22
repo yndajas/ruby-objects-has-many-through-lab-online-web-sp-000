acafdc0ee58c8a91fc9e72bdf4421eeb298d30c1
Ynda Jas <yndajas@gmail.com>
2020-07-22 16:25:34 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,19 @@
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, doctor)
+    appointment = Appointment.new
+  end
+  
+end
