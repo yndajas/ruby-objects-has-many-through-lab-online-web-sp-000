64c2d3e1bd280228195de94cb935fc4e094b08f4
Ynda Jas <yndajas@gmail.com>
2020-07-22 16:30:25 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,14 +2,14 @@ class Artist
   attr_accessor :name
   
   @@all = []
-  
+
+  def self.all
+    @@all
+  end
+
   def initialize(name)
     @name = name
     @@all << self
-  end
-  
-  def self.all
-    @@all
   end
   
   def new_song(name, genre)
